75e84af697fcb2984949d3556e8f33d4374fd3fe
komputerboy <42455711+komputerboy@users.noreply.github.com>
2021-10-22 15:01:32 +0700

    Update ipsec.sh

--- kebu/ipsec.sh
+++ kebu/ipsec.sh
@@ -276,13 +276,13 @@ mkdir -p /run/pluto
 service fail2ban restart 2>/dev/null
 service ipsec restart 2>/dev/null
 service xl2tpd restart 2>/dev/null
-wget -O /usr/bin/add-l2tp https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kebu/add-l2tp.sh && chmod +x /usr/bin/add-l2tp
-wget -O /usr/bin/del-l2tp https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kebu/del-l2tp.sh && chmod +x /usr/bin/del-l2tp
-wget -O /usr/bin/add-pptp https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kebu/add-pptp.sh && chmod +x /usr/bin/add-pptp
-wget -O /usr/bin/del-pptp https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kebu/del-pptp.sh && chmod +x /usr/bin/del-pptp
-wget -O /usr/bin/cek-pptp https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kebu/cek-pptp.sh && chmod +x /usr/bin/cek-pptp
-wget -O /usr/bin/renew-pptp https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kebu/renew-pptp.sh && chmod +x /usr/bin/renew-pptp
-wget -O /usr/bin/renew-l2tp https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kebu/renew-l2tp.sh && chmod +x /usr/bin/renew-l2tp
+wget -O /usr/bin/add-l2tp https://raw.githubusercontent.com/komputerboy/komputerboy/main/kebu/add-l2tp.sh && chmod +x /usr/bin/add-l2tp
+wget -O /usr/bin/del-l2tp https://raw.githubusercontent.com/komputerboy/komputerboy/main/kebu/del-l2tp.sh && chmod +x /usr/bin/del-l2tp
+wget -O /usr/bin/add-pptp https://raw.githubusercontent.com/komputerboy/komputerboy/main/kebu/add-pptp.sh && chmod +x /usr/bin/add-pptp
+wget -O /usr/bin/del-pptp https://raw.githubusercontent.com/komputerboy/komputerboy/main/kebu/del-pptp.sh && chmod +x /usr/bin/del-pptp
+wget -O /usr/bin/cek-pptp https://raw.githubusercontent.com/komputerboy/komputerboy/main/kebu/cek-pptp.sh && chmod +x /usr/bin/cek-pptp
+wget -O /usr/bin/renew-pptp https://raw.githubusercontent.com/komputerboy/komputerboy/main/kebu/renew-pptp.sh && chmod +x /usr/bin/renew-pptp
+wget -O /usr/bin/renew-l2tp https://raw.githubusercontent.com/komputerboy/komputerboy/main/kebu/renew-l2tp.sh && chmod +x /usr/bin/renew-l2tp
 touch /var/lib/premium-script/data-user-l2tp
 touch /var/lib/premium-script/data-user-pptp
 
